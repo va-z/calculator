24037b72e00087c0a00ee07b1ce56fdfb578fda0
va-z <vasilizadorojnyuk@gmail.com>
2021-02-13 22:55:27 +0500

    init: start calculator project

---
+++ src/js/example.js
@@ -0,0 +1,8 @@
+const example = () => {
+  return (
+    'Sensible webpack 5 boilerplate using Babel and PostCSS ' +
+    'with a hot dev server and an optimized production build.'
+  );
+};
+
+export default example;
