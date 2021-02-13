d16d54906bd48ea321040528b1445254f4297eee
va-z <vasilizadorojnyuk@gmail.com>
2021-02-14 01:05:17 +0500

    feat: add variables

--- src/js/example.js
+++
@@ -1,8 +0,0 @@
-const example = () => {
-  return (
-    'Sensible webpack 5 boilerplate using Babel and PostCSS ' +
-    'with a hot dev server and an optimized production build.'
-  );
-};
-
-export default example;
